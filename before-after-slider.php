1e33143e0a4971c0c21ef42d24476db25be115c0
Shuhei Nishimura <nishimura@digitalcube.jp>
2016-10-04 16:23:56 +0900

    fix comparison slider resize handle

--- before-after-slider.php
+++ before-after-slider.php
@@ -13,17 +13,15 @@
  */
 
 /**
+ * Add image size.
+ */
+add_image_size( 'thmb_1020_500', 1020, 500, true );
+add_image_size( 'thumb_509_999', 509, 9999, true );
+
+/**
  * Add Pinterest Pins and Boards :)
  */
 function add_ba_slider_script() {
-
-	wp_enqueue_script(
-		'bfore-after-js',
-		plugins_url( 'js/before-after.min.js' , __FILE__ ),
-		array( 'jquery' ),
-		'',
-		true
-	);
 
 	wp_enqueue_script(
 		'bxslider-js',
@@ -34,26 +32,19 @@ function add_ba_slider_script() {
 	);
 
 	wp_enqueue_script(
-		'easing-js',
-		plugins_url( 'js/jquery.easing.1.3.js' , __FILE__ ),
+		'bfore-after-js',
+		plugins_url( 'js/before-after.min.js' , __FILE__ ),
 		array( 'jquery' ),
 		'',
 		true
 	);
 
 	wp_enqueue_script(
-		'fitvids-js',
-		plugins_url( 'js/jquery.fitvids.js' , __FILE__ ),
-		array( 'jquery' ),
-		'',
+		'bfore-after-common-js',
+		plugins_url( 'js/common.js' , __FILE__ ),
+		array( 'jquery', 'bfore-after-js' ),
+		date( 'TmdHis', filemtime( plugin_dir_path( __FILE__ ) . 'js/common.js' ) ),
 		true
-	);
-
-	wp_enqueue_style(
-		'before-after-style',
-		plugins_url( 'css/before-after.css' , __FILE__ ),
-		'',
-		''
 	);
 
 	wp_enqueue_style(
@@ -63,5 +54,89 @@ function add_ba_slider_script() {
 		''
 	);
 
+	wp_enqueue_style(
+		'common-style',
+		plugins_url( 'css/common.css' , __FILE__ ),
+		'',
+		''
+	);
+
 }
 add_action( 'wp_enqueue_scripts', 'add_ba_slider_script' );
+
+
+function get_ba_image_slider_content( $atts ) {
+
+	global $post;
+	extract( shortcode_atts( array(
+		'class'          => 'bxslider',
+		'post_type'      => 'works',
+		'posts_per_page' => 999,
+	), $atts ) );
+
+	//var_dump( $post_type );
+
+	if ( is_singular( $post_type ) ) {
+
+		$post_id = get_the_ID();
+		$post_data = get_post( $post_id );
+		$post_title = esc_html( $post_data->post_title );
+		$ba_images_count = get_post_meta( $post_id, 'image_ba', true );
+		//var_dump( $ba_images_count );
+
+		$html  = '';
+
+		$html .= '<div class="slider-area">';
+		$html .= '<ul class="bxslider">';
+
+		for ( $i = 0; $i < intval( $ba_images_count ); $i++ ) {
+
+			$html .= '<li>';
+
+			$b_image_id       = get_post_meta( $post_id, sprintf( 'image_ba_%d__b', $i ), true );
+			$before_image     = wp_get_attachment_image_src( $b_image_id, 'thmb_1020_500' );
+			$before_image_tag = sprintf(
+				'<img src="%1$s" width="%2$d" height="%3$d" alt="%4$s" title="%4$s">',
+				esc_url( $before_image[ 0 ] ),
+				intval( $before_image[ 1 ] ),
+				intval( $before_image[ 2 ] ),
+				$post_title . ' 施工前_' . intval( $i + 1 )
+			);
+
+			//$html .= $before_image_tag;
+
+			$a_image_id      = get_post_meta( $post_id, sprintf( 'image_ba_%d__a', $i ), true );
+			$after_image     = wp_get_attachment_image_src( $a_image_id, 'thmb_1020_500' );
+			$after_image_tag = sprintf(
+				'<img src="%1$s" width="%2$d" height="%3$d" alt="%4$s" title="%4$s">',
+				esc_url( $after_image[ 0 ] ),
+				intval( $after_image[ 1 ] ),
+				intval( $after_image[ 2 ] ),
+				$post_title . ' 施工後_' . intval( $i + 1 )
+			);
+
+			//$html .= $after_image_tag;
+
+
+			$html .= '<div class="ba-slider">';
+			$html .= $before_image_tag;
+			$html .= '<div class="resize">';
+			$html .= $after_image_tag;
+			$html .= '</div>';
+			$html .= '<span class="handle"></span>';
+			$html .= '</div>';
+
+
+			$html .= '</li>';
+
+		}
+
+		$html .= '</ul>';
+		$html .= '</div>';
+
+		return $html;
+
+	}
+
+}
+add_shortcode( 'ba_slider', 'get_ba_image_slider_content' );
--- js/common.js
+++ js/common.js
@@ -1,57 +1,145 @@
 (function($){
 
-  $(window).on('load', function(){
-    var slider = $('.bxslider').bxSlider({
-      auto: true,
-      autoHover: true,
-      adaptiveHeight: true,
-      tickerHover: true,
-      useCSS: false,
-      onSliderLoad: function ( currentIndex ) {
-        $('.ba-slider').css({
-          display: 'block'
-        });
-      },
-      onSlideAfter: function ( $slideElement, oldIndex, newIndex ) {
-        if( $slideElement.find('ba-slider') ) {
-          var base = $slideElement.find('.ba-slider');
-          var resize = base.find('.resize');
-          var handle = base.find('.handle');
-          resize.not(':animated').animate({
-            width: '55%'
-          }, 50, 'linear', function(){
-            $(this).not(':animated').animate({
-              width: '45%'
-            });
-            recovery($(this));
-          });
-          handle.not(':animated').animate({
-            left: '55%'
-          }, 50, 'linear', function(){
-            $(this).not(':animated').animate({
-              left: '45%'
-            });
-            recovery($(this));
-          });
-        }
-        slider.stopAuto();
-        slider.startAuto();
-      }
-    });
-    $('.ba-slider').beforeAfter();
-  });
+	//$(window).on('load', function(){
 
-  function recovery ( elm ) {
-    var checkClass = elm.attr('class');
-    if ( checkClass === 'resize' ) {
-      elm.not(':animated').animate({
-        width: '50%'
-      });
-    } else {
-      elm.not(':animated').animate({
-        left: '50%'
-      });
-    }
-  }
+		var winW = $( window ).width();
+		var moveFlag = false;
+
+		if ( winW > 799 ) {
+			var slider = $( '.bxslider' ).bxSlider({
+				auto: true,
+				autoHover: true,
+				//adaptiveHeight: false,
+				tickerHover: true,
+				useCSS: false,
+				touchEnabled: false,
+				oneToOneTouch: false,
+				onSliderLoad: function ( currentIndex ) {
+
+					var $slideElement = $( '.bxslider li' ).not( '.bx-clone' ).eq( currentIndex );
+					var base = $slideElement.find('.ba-slider');
+					if ( $slideElement.find( 'ba-slider' ) && moveFlag == false ) {
+						var base = $slideElement.find('.ba-slider');
+						var resize = base.find('.resize');
+						var handle = base.find('.handle');
+						resize.not(':animated').animate({
+							width: '55%'
+						}, 50, 'linear', function () {
+							$(this).not(':animated').animate({
+								width: '45%'
+							});
+							recovery($(this));
+						});
+						handle.not(':animated').animate({
+							left: '55%'
+						}, 50, 'linear', function () {
+							$(this).not(':animated').animate({
+								left: '45%'
+							});
+							recovery($(this));
+						});
+					}
+
+					moveFlag = true;
+
+				},
+				onSlideAfter: function ( $slideElement, oldIndex, newIndex ) {
+					slider.stopAuto();
+					slider.startAuto();
+					var base = $slideElement.find('.ba-slider');
+					var resize = base.find('.resize');
+					var handle = base.find('.handle');
+					recovery_soon( resize );
+					recovery_soon( handle );
+				}
+			});
+
+		} else {
+
+			var slider = $('.bxslider').bxSlider({
+				auto: false,
+				autoHover: true,
+				tickerHover: true,
+				useCSS: false,
+				touchEnabled: false,
+				oneToOneTouch: false,
+				onSliderLoad: function ( currentIndex ) {
+
+					var $slideElement = $( '.bxslider li' ).not( '.bx-clone' ).eq( currentIndex );
+					var base = $slideElement.find('.ba-slider');
+					if ( $slideElement.find( 'ba-slider' ) && moveFlag == false ) {
+						var base = $slideElement.find('.ba-slider');
+						var resize = base.find('.resize');
+						var handle = base.find('.handle');
+						resize.not(':animated').animate({
+							width: '55%'
+						}, 50, 'linear', function () {
+							$(this).not(':animated').animate({
+								width: '45%'
+							});
+							recovery($(this));
+						});
+						handle.not(':animated').animate({
+							left: '55%'
+						}, 50, 'linear', function () {
+							$(this).not(':animated').animate({
+								left: '45%'
+							});
+							recovery($(this));
+						});
+					}
+
+					moveFlag = true;
+
+				},
+				onSlideAfter: function ( $slideElement, oldIndex, newIndex ) {
+
+					var base = $slideElement.find('.ba-slider');
+					var resize = base.find('.resize');
+					var handle = base.find('.handle');
+					recovery_soon( resize );
+					recovery_soon( handle );
+
+				}
+
+			});
+
+		}
+		$('.ba-slider').beforeAfter();
+	//});
+
+	function recovery ( elm ) {
+
+		var checkClass = elm.attr('class');
+		if ( checkClass === 'resize' ) {
+
+			elm.not(':animated').animate({
+				width: '50%'
+			});
+
+		} else {
+
+			elm.not(':animated').animate({
+				left: '50%'
+			});
+
+		}
+
+	}
+
+	function recovery_soon( elm ) {
+
+		$( '.bxslider li' ).each( function ( e ) {
+
+			var resizeTarget = $( this ).find( '.resize' );
+			var handleTarget = $( this ).find( '.handle' );
+			$( resizeTarget ).not( ':animated' ).animate({ width: '50%' });
+			$( handleTarget ).not( ':animated' ).animate({ left: '50%' });
+			$( resizeTarget ).removeAttr( 'style' );
+			$( handleTarget ).removeAttr( 'style' );
+
+		});
+
+	}
 
 })(jQuery);
